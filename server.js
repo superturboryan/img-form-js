7225fab6deaa8b7c0780264e6fb14e6864dda107
Ryan David Forsyth <forsyth.r@gmail.com>
2019-04-11 14:29:23 -0400

    Update multer object to upload multiple files at once

--- server.js
+++ server.js
@@ -35,12 +35,19 @@ let makePage = () => {
         </head>
         <body class='avenir bg-light-gray'>
             <h1 class="tc">Let's upload some images!</h1>
-            <div class="mh7 tc ba mb4 pa3">From <u>local</u> directory:
+            <div class="mh7 tc ba mb4 pa3">From <u>local</u> directory:<br/>(select up to 3 files)
                 <form class="pa3" action='/image' method="POST" enctype="multipart/form-data">
                     
-                    <input style='cursor: pointer;' type='file' name='funny-image'></input>
-                    <input type='text' name='title' placeholder='Title'></input>
-                    <input type='text' name='desc' placeholder='Description'></input>
+                    <input style='cursor: pointer;' type='file' name='up-images' multiple></input>
+                    <br/>
+                    <input type='text' name='title1' placeholder='Image 1 Title'></input>
+                    <input type='text' name='desc1' placeholder='Description'></input>
+                    <br/>
+                    <input type='text' name='title2' placeholder='Image 2 Title'></input>
+                    <input type='text' name='desc2' placeholder='Description'></input>
+                    <br/>
+                    <input type='text' name='title3' placeholder='Image 3 Title'></input>
+                    <input type='text' name='desc3' placeholder='Description'></input>
                     <br/>
                     <button style='cursor: pointer;' class="mt2" type='submit'>Upload</button>
                     
@@ -49,15 +56,15 @@ let makePage = () => {
             <div class="mh7 tc ba mb4 pa3">or from <u>Internet</u> URLs:
                 <form class="pa3" action='/url' method="POST" enctype="multipart/form-data">
                     1) <input class='w-third' type='text' name='path1' placeholder='Enter URL here'></input>
-                    <input type='text' name='title1' placeholder='Title'></input>
+                    <input type='text' name='title1' placeholder='Image 1 Title'></input>
                     <input type='text' name='desc1' placeholder='Description'></input>
                     <br/>
                     2) <input class='w-third' type='text' name='path2' placeholder='Enter URL here'></input>
-                    <input type='text' name='title2' placeholder='Title'></input>
+                    <input type='text' name='title2' placeholder='Image 2 Title'></input>
                     <input type='text' name='desc2' placeholder='Description'></input>
                     <br/>
                     3) <input class='w-third' type='text' name='path3' placeholder='Enter URL here'></input>
-                    <input type='text' name='title3' placeholder='Title'></input>
+                    <input type='text' name='title3' placeholder='Image 3 Title'></input>
                     <input type='text' name='desc3' placeholder='Description'></input>
                     <br/>
                     <button style='cursor: pointer;' class="mt2" type='submit'>Upload</button>
@@ -82,48 +89,93 @@ app.get("/", (req, res) => {
 });
 
 app.post("/url", upload.none(), (req, res) => {
-  posts.push({
-    path: req.body.path1,
-    title: req.body.title1,
-    desc: req.body.desc1
-  });
+  if (req.body.path1 !== "") {
+    posts.push({
+      path: req.body.path1,
+      title: req.body.title1,
+      desc: req.body.desc1
+    });
+  }
 
-  posts.push({
-    path: req.body.path2,
-    title: req.body.title2,
-    desc: req.body.desc2
-  });
+  if (req.body.path2 !== "") {
+    posts.push({
+      path: req.body.path2,
+      title: req.body.title2,
+      desc: req.body.desc2
+    });
+  }
 
-  posts.push({
-    path: req.body.path3,
-    title: req.body.title3,
-    desc: req.body.desc3
-  });
+  if (req.body.path3 !== "") {
+    posts.push({
+      path: req.body.path3,
+      title: req.body.title3,
+      desc: req.body.desc3
+    });
+  }
 
   res.send(makePage());
 });
 
-app.post("/image", upload.single("funny-image"), (req, res) => {
-  //
-  if (req.file === undefined) return;
+app.post("/image", upload.array("up-images", 1), (req, res) => {
+  if (req.files[0] !== undefined) {
+    //Create var for file1 of request
+    let file1 = req.files[0];
+    //Get the extension of the file
+    let ext1 = file1.originalname.split(".").pop();
+    //Create a new filename for the stored uploads
+    let newFileName1 = `${file1.filename}.${ext1}`;
+    //Rename file path
+    fs.renameSync(file1.path, `${__dirname}/uploads/${newFileName1}`);
+    //Create path for user to access file
+    let frontendPath1 = `/images/${newFileName1}`;
+    //Push the path+title object to the array of posts
 
-  //Create var for file in the body
-  let file = req.file;
-  //Get the extension of the file
-  let ext = file.originalname.split(".").pop();
-  //Create a new filename for the stored uploads
-  let newFileName = `${file.filename}.${ext}`;
-  //Rename file path
-  fs.renameSync(file.path, `${__dirname}/uploads/${newFileName}`);
-  //Create path for user to access file
-  let frontendPath = `/images/${newFileName}`;
-  //Push the path+title object to the array of posts
+    posts.push({
+      path: frontendPath1,
+      title: req.body.title1,
+      desc: req.body.desc1
+    });
+  }
 
-  posts.push({
-    path: frontendPath,
-    title: req.body.title,
-    desc: req.body.desc
-  });
+  if (req.files[1] !== undefined) {
+    //Create var for file2 of request
+    let file2 = req.files[1];
+    //Get the extension of the file
+    let ext2 = file2.originalname.split(".").pop();
+    //Create a new filename for the stored uploads
+    let newFileName2 = `${file2.filename}.${ext2}`;
+    //Rename file path
+    fs.renameSync(file2.path, `${__dirname}/uploads/${newFileName2}`);
+    //Create path for user to access file
+    let frontendPath2 = `/images/${newFileName2}`;
+    //Push the path+title object to the array of posts
+
+    posts.push({
+      path: frontendPath2,
+      title: req.body.title2,
+      desc: req.body.desc2
+    });
+  }
+
+  if (req.files[2] !== undefined) {
+    //Create var for file3 of request
+    let file3 = req.files[2];
+    //Get the extension of the file
+    let ext3 = file3.originalname.split(".").pop();
+    //Create a new filename for the stored uploads
+    let newFileName3 = `${file3.filename}.${ext3}`;
+    //Rename file path
+    fs.renameSync(file3.path, `${__dirname}/uploads/${newFileName3}`);
+    //Create path for user to access file
+    let frontendPath3 = `/images/${newFileName3}`;
+    //Push the path+title object to the array of posts
+
+    posts.push({
+      path: frontendPath3,
+      title: req.body.title3,
+      desc: req.body.desc3
+    });
+  }
 
   //Reload the page
   res.send(makePage());
